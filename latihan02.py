7eb3c1d9113f74e82b73d88b6dbec323fe62c86d
bejonot <root@LAPTOP-J9MNB7D0.localdomain>
2020-11-10 14:33:41 +0700

    latihan02.py filled

--- latihan02.py
+++ latihan02.py
@@ -1,4 +1,6 @@
 listName = ["Luke Skywalker", "Padme Amdala", "Qul-Gon Jimn"]
+#listName = [0, 1, 2] ini index
 print(listName)
-for variable in listName:
+for variable in listName: #untuk variable di dalam listName, pindahin index pertamanya disini
     print(variable)
+#variabel nangkep list index pertama
